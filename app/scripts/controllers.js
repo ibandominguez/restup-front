573676a816c1623d87ca5a0c940748a0b8003260
ibandominguez <ibandominguez@live.com>
2016-10-13 17:32:35 +0100

    implementing creating and update methods

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -3,8 +3,6 @@
 angular.module('restup-front.controllers', [])
 
 .controller('AppCtrl', ['$scope', 'resource', '$state', 'localStorageService', '$timeout', function($scope, resource, $state, localStorageService, $timeout) {
-  $scope.toastMessage = null;
-
   $scope.getResourceFields = function(title) {
     if (!$scope.resources) {
       return;
@@ -18,11 +16,7 @@ angular.module('restup-front.controllers', [])
   };
 
   $scope.toast = function(message, time) {
-    $scope.toastMessage = message;
-
-    $timeout(function() {
-      $scope.toastMessage = null;
-    }, time || 3000);
+    alert(message);
   };
 
   resource.query('/resources')
@@ -35,12 +29,46 @@ angular.module('restup-front.controllers', [])
     });
 }])
 
-.controller('ResourceCtrl', ['$scope', 'resource', '$stateParams', function($scope, resource, $stateParams) {
+.controller('ResourceCtrl', ['$scope', 'resource', '$stateParams', '$ionicModal', function($scope, resource, $stateParams, $ionicModal) {
   $scope.results = [];
   $scope.fields = null;
+  $scope.selectedItem = null;
   $scope.selectedResource = $stateParams.resource;
 
-  resource.query('/' + $scope.selectedResource)
+  $ionicModal.fromTemplateUrl('views/modals/manage.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.manage = function(item) {
+    $scope.selectedItem = item;
+    $scope.modal.show();
+  };
+
+  $scope.updateOrCreate = function(item) {
+    resource.updateOrCreate($scope.selectedResource, item)
+      .success(function(res) {
+        $scope.modal.hide();
+      })
+      .error(function(err) {
+        $scope.toast(err && err.error);
+      });
+  };
+
+  $scope.remove = function(item) {
+    resource.remove($scope.selectedResource, item)
+      .success(function(res) {
+        $scope.modal.hide();
+        $scope.results.splice($scope.results.indexOf(item), 1);
+      })
+      .error(function(err) {
+        $scope.toast('Error connecting with the rest api');
+      });
+  };
+
+  resource.query($scope.selectedResource)
     .success(function(results) {
       $scope.results = results;
       $scope.fields = $scope.getResourceFields($stateParams.resource);
